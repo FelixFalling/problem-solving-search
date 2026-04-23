1c29c98b2f463e0889c9f85693b428513d815476
Nick <166885937+FelixFalling@users.noreply.github.com>
2026-04-22 23:37:26 -0700

    init the uv setup

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from problem-solving-search!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ problem_solving_as_search.py
@@ -0,0 +1,3 @@
+class ProblemSolvingSearch:
+    board_config = [4, 5, "b", 6, 1, 8, 7, 3, 2]
+    goal_board_config = [1, 2, 3, 4, 5, 6, 7, 8, "b"]
